96ca646fca0958b0a04dbf0b12e99a64e9ab8412
Matteo Depalo <matteodepalo@gmail.com>
2016-08-20 18:50:43 +0100

    Remove moment-shim

--- blueprints/ember-bootstrap-datetimepicker/index.js
+++ blueprints/ember-bootstrap-datetimepicker/index.js
@@ -2,8 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonToProject('ember-cli-moment-shim', '~0.6.1').then(function() {
-      return this.addPackageToProject('eonasdan-bootstrap-datetimepicker', '~4.15.35');
-    }.bind(this));
+    return this.addPackageToProject('eonasdan-bootstrap-datetimepicker', '4.17.37');
   }
 };
